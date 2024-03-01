33078177abda7617e60c4069956d2edf7bcd3f82
Dabrech <brianlh21@gmail.com>
2024-02-29 23:07:16 -0300

    actualizacion

--- js/main.js
+++ js/main.js
@@ -1,27 +1,35 @@
-let r = 5000;
-let s = 4500;
-let g = 2000;
-let totalP;
-let compra
+let precioRemeras = 5000;
+let precioSudaderas = 4500;
+let precioGorras = 2000;
+let totalP = 0;
+let continuarComprando = true;
 
+alert("Bienvenidos a TodoModa. Por favor, elija los productos para agregar al carrito de compras.");
 
-alert("Bienvenidos a TodoModa, se les avisa que solo pueden comprar hasta 10 prendas")
-for (let x = 0; x <= 10; x++) {
-    compra = prompt("Ingrese el producto que quiera comprar: r = remeras, s = sudaderas, g = gorras, en caso de que no quiera comprar coloque: no , y en el caso de cancelar su compra coloque: cancelar")
-    if (compra.toLowerCase = "r") {
-        totalP + r
-        
-    } else if(compra.toLowerCase = s) {
-        totalP + s
-        
-    }else if(compra.toLowerCase = g){
-        totalP + g
+while (continuarComprando) {
+    const productoElegido = prompt("Ingrese el número del producto que desea comprar:\n1. Remeras\n2. Sudaderas\n3. Gorras\n\nSi desea finalizar la compra, por favor ingrese 0:");
 
-    }else if(compra.toLowerCase = "no"){
-        alert("El total de su compra es de "+ totalP)
-
-    }else if(compra.toLowerCase = "cancelar"){
-        break
+    switch (productoElegido) {
+        case "1":
+            totalP += precioRemeras;
+            alert("Excelente, usted ha agregado una remera.");
+            break;
+        case "2":
+            totalP += precioSudaderas;
+            alert("Excelente, usted ha agregado una sudadera.");
+            break;
+        case "3":
+            totalP += precioGorras;
+            alert("Excelente, usted ha agregado una gorra.");
+            break;
+        case "0":
+            continuarComprando = false;
+            break;
+        default:
+            alert("Opción inválida. Por favor, elija un número válido o escriba '0' para finalizar la compra.");
     }
-    
+}
+
+if (totalP > 0) {
+    const opcionFinal = confirm(`El total es de $${totalP}. Muchas gracias por su compra! Hasta luego!`);
 }
